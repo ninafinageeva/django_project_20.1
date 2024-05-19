0df1aebf53d8e3f6dbf2638c509f527b24432efe
ninafinageeva <finageeva.nina@mail.ru>
2024-05-19 12:26:39 +0300

    не получается запустить сервер

--- src/catalog/views.py
+++ src/catalog/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
-def index(request):
-    return render(request, 'catalog/base.html')
+from src.catalog.models import Product
+
+
+# контроллеры для сайта
+
+def product_list(request):
+    """Выводит список товаров на сайте"""
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'catalog/product_list.html', context)
+
+
+def product_detail(request, pk):
+    product_pk = Product.objects.get(pk=pk)
+    context = {'product': product_pk}
+    return render(request, 'catalog/product_detail.html', context)
 
 
 def contacts(request):
+    """Принимает контактные данные от пользователя с сайта"""
     if request.method == 'POST':
         # в переменной request хранится информация о методе, который отправлял пользователь
         name = request.POST.get('name')
